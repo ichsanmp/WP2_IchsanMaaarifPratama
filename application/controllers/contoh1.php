8c2e7b91ec3538c33153a26d2c58847dd4518873
ichsanmp <impratama04@gmail.com>
2022-09-18 09:51:03 +0700

    Latihan Pertemuan pertama

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo"<h1>Perkenalkan</h1>";
+		echo"Nama Saya Ichsan Maa'arif Pratama<br>
+		NIM saya 19210117<br>
+		Saya tinggal di Tambun Utara";
+	}
+}
